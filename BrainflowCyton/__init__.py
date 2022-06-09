7f9032b0317b05c7cd6b61903910fa36ab50c805
zeyus <zeyus@zeyus.com>
2022-06-09 11:36:49 +0200

    bump version

--- BrainflowCyton/__init__.py
+++ BrainflowCyton/__init__.py
@@ -4,5 +4,5 @@ BrainflowCyton.
 Python wrapper for BrainFlow API + Cyton SDK for collecting realtime or offline (SDCard) EEG, EMG, EKG data.
 """
 
-__version__ = "0.1.0"
+__version__ = '0.1.1'
 __author__ = 'zeyus'
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='BrainflowCyton',
-    version='0.1.0',    
+    version='0.1.1',    
     description='Python wrapper for BrainFlow API + Cyton SDK for collecting realtime or offline (SDCard) EEG, EMG, EKG data.',
     long_description=long_description,
     long_description_content_type="text/markdown",
